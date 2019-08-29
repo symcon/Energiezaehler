4fdc82498df6dedb84186d0c6b7555066936afb3
Till Brede <tb@symcon.de>
2019-08-29 10:59:59 +0200

    applied cs fixer style

--- EnergiezaehlerStrom/module.php
+++ EnergiezaehlerStrom/module.php
@@ -1,59 +1,57 @@
-<?
+<?php
 
-	class EnergiezaehlerStrom extends IPSModule
-	{
-		
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
+    class EnergiezaehlerStrom extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
 
-			/*
-			 * 0 = Power (W)
-			 * 1 = Current (A)
-			 */
+            /*
+             * 0 = Power (W)
+             * 1 = Current (A)
+             */
             $this->RegisterPropertyInteger("SourceType", 0);
-			$this->RegisterPropertyInteger("SourceVariable", 0);
+            $this->RegisterPropertyInteger("SourceVariable", 0);
             $this->RegisterPropertyInteger("Voltage", 230);
-			$this->RegisterPropertyInteger("Interval", 60);
-			
-			$this->RegisterTimer("UpdateTimer", 0, 'EZS_Update($_IPS[\'TARGET\']);');
-			
-			$this->RegisterVariableFloat("Current", "Current", "Watt.3680", 0);
-			$this->RegisterVariableFloat("Counter", "Counter", "Electricity", 1);
-		}
-	
-		public function ApplyChanges()
-		{
-			
-			//Never delete this line!
-			parent::ApplyChanges();
+            $this->RegisterPropertyInteger("Interval", 60);
+            
+            $this->RegisterTimer("UpdateTimer", 0, 'EZS_Update($_IPS[\'TARGET\']);');
+            
+            $this->RegisterVariableFloat("Current", "Current", "Watt.3680", 0);
+            $this->RegisterVariableFloat("Counter", "Counter", "Electricity", 1);
+        }
+    
+        public function ApplyChanges()
+        {
+            
+            //Never delete this line!
+            parent::ApplyChanges();
 
-			$this->SetTimerInterval("UpdateTimer", $this->ReadPropertyInteger("Interval")*1000);
+            $this->SetTimerInterval("UpdateTimer", $this->ReadPropertyInteger("Interval")*1000);
 
             $this->RegisterMessage($this->ReadPropertyInteger("SourceVariable"), VM_UPDATE);
-
-		}
-
-        public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
-
-			//guard against messages that were registered from previous configurations
-			if($SenderID == $this->ReadPropertyInteger("SourceVariable")) {
-				$this->Update();
-			}
-
         }
-	
-		/**
-		* This function will be available automatically after the module is imported with the module control.
-		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
-		*
-		* EZS_Update($id);
-		*
-		*/
-		public function Update()
-		{
-			if(IPS_SemaphoreEnter("EZS_" . $this->InstanceID, 1000)) {
+
+        public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+        {
+
+            //guard against messages that were registered from previous configurations
+            if ($SenderID == $this->ReadPropertyInteger("SourceVariable")) {
+                $this->Update();
+            }
+        }
+    
+        /**
+        * This function will be available automatically after the module is imported with the module control.
+        * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+        *
+        * EZS_Update($id);
+        *
+        */
+        public function Update()
+        {
+            if (IPS_SemaphoreEnter("EZS_" . $this->InstanceID, 1000)) {
                 if (IPS_VariableExists($this->ReadPropertyInteger("SourceVariable"))) {
 
                     //we use the last updated value to calculate the amount the need to add
@@ -77,13 +75,8 @@
                     }
 
                     SetValue($this->GetIDForIdent("Current"), $sourceValue);
-
                 }
                 IPS_SemaphoreLeave("EZS_" . $this->InstanceID);
             }
-		
-		}
-	
-	}
-
-?>
+        }
+    }
