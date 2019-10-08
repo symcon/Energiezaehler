b0460a5070e284d3e7e2a4b43ac0117375ad6148
Till Brede <tb@symcon.de>
2019-10-08 13:40:52 +0200

    added referencing for EnergiezaehlerStrom

--- EnergiezaehlerStrom/module.php
+++ EnergiezaehlerStrom/module.php
@@ -33,6 +33,16 @@ declare(strict_types=1);
             $this->SetTimerInterval('UpdateTimer', $this->ReadPropertyInteger('Interval') * 1000);
 
             $this->RegisterMessage($this->ReadPropertyInteger('SourceVariable'), VM_UPDATE);
+        
+            //Add references
+            foreach ($this->GetReferenceList() as $reference) {
+                $this->UnregisterReference($reference);
+            }
+            $sourceID = $this->ReadPropertyInteger('SourceVariable');
+            if ($sourceID != 0) {
+                $this->RegisterReference($sourceID);
+            }
+            
         }
 
         public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
