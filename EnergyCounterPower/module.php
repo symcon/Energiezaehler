2688dc00d30080d09d60469da7e77f4c604eca64
1Rolf <121196961+1Rolf@users.noreply.github.com>
2025-02-02 21:02:41 +0100

    ratchet

--- EnergyCounterPower/module.php
+++ EnergyCounterPower/module.php
@@ -22,6 +22,8 @@ class EnergyCounterPower extends IPSModule
 
         $this->RegisterVariableFloat('Current', $this->Translate('Current'), 'Watt.3680', 0);
         $this->RegisterVariableFloat('Counter', $this->Translate('Counter'), 'Electricity', 1);
+
+        $this->RegisterVariableBoolean('Ratchet', false);
     }
 
     public function ApplyChanges()
@@ -74,8 +76,13 @@ class EnergyCounterPower extends IPSModule
                 //we use the last updated value to calculate the amount the need to add
                 $timeDiff = time() - IPS_GetVariable($this->GetIDForIdent('Current'))['VariableUpdated'];
 
-                //get current value
+                //get current value -> if Ratchet is active and value is negativ set to 0
+                $ratchet = $this->ReadPropertyBoolean("Ratchet");
                 $currentValue = GetValue($this->GetIDForIdent('Current'));
+                if(($ratchet) && ($currentValue<0))
+                {
+                    $currentValue = 0;
+                }
 
                 //fetch last counter
                 $lastCounterValue = GetValue($this->GetIDForIdent('Counter'));
