e37a7c4d8c325a5e01e2c64e50722d59a7885f70
1Rolf <121196961+1Rolf@users.noreply.github.com>
2025-02-04 21:49:09 +0100

    add Backstop/Rücklaufsperre to power counter (#19)

--- EnergyCounterPower/module.php
+++ EnergyCounterPower/module.php
@@ -17,11 +17,13 @@ class EnergyCounterPower extends IPSModule
         $this->RegisterPropertyInteger('SourceVariable', 0);
         $this->RegisterPropertyInteger('Voltage', 230);
         $this->RegisterPropertyInteger('Interval', 60);
+        $this->RegisterPropertyBoolean('Backstop', false);
 
         $this->RegisterTimer('UpdateTimer', 0, 'EZS_Update($_IPS[\'TARGET\']);');
 
         $this->RegisterVariableFloat('Current', $this->Translate('Current'), 'Watt.3680', 0);
         $this->RegisterVariableFloat('Counter', $this->Translate('Counter'), 'Electricity', 1);
+
     }
 
     public function ApplyChanges()
@@ -74,8 +76,12 @@ class EnergyCounterPower extends IPSModule
                 //we use the last updated value to calculate the amount the need to add
                 $timeDiff = time() - IPS_GetVariable($this->GetIDForIdent('Current'))['VariableUpdated'];
 
-                //get current value
+                //get current value -> if Backstop is active and value is negativ set to 0
+                $backstop = $this->ReadPropertyBoolean('Backstop');
                 $currentValue = GetValue($this->GetIDForIdent('Current'));
+                if ($backstop && ($currentValue < 0)) {
+                    $currentValue = 0;
+                }
 
                 //fetch last counter
                 $lastCounterValue = GetValue($this->GetIDForIdent('Counter'));
